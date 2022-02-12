a8006a1164e28f67400b855be06e505abbc09e31
Andres Lopez <alopez@booz.cl>
2022-02-12 01:32:17 -0500

    feat: Register Patient

--- app/src/main/java/com/example/monitoringapp/ui/doctor/EditPatientActivity.kt
+++ app/src/main/java/com/example/monitoringapp/ui/doctor/EditPatientActivity.kt
@@ -1,17 +1,18 @@
 package com.example.monitoringapp.ui.doctor
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.monitoringapp.databinding.ActivityEditPatientBinding
 import com.example.monitoringapp.databinding.ActivitySearchPatientBinding
 
-class SearchPatientActivity : AppCompatActivity() {
+class EditPatientActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySearchPatientBinding
+    private lateinit var binding: ActivityEditPatientBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivitySearchPatientBinding.inflate(layoutInflater)
+        binding = ActivityEditPatientBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
     }
